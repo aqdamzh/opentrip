71617c8f40793cae12cbfc82f8a384752917bc39
aqdamzh <aqdamzain1@gmail.com>
2019-12-10 16:38:08 +0700

    clean some page

--- application/views/home.php
+++ application/views/home.php
@@ -112,7 +112,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
     <div class="slide-one-item home-slider owl-carousel">
       
-      <div class="site-blocks-cover overlay" style="background-image: url(images/hero_bg_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+      <div class="site-blocks-cover overlay" style="background-image: url(<?php echo base_url() ?>assets/images/hero_bg_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
         <div class="container">
           <div class="row align-items-center justify-content-center text-center">
 
@@ -128,7 +128,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </div>
       </div>  
 
-      <div class="site-blocks-cover overlay" style="background-image: url(images/hero_bg_2.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+      <div class="site-blocks-cover overlay" style="background-image: url(<?php echo base_url() ?>assets/images/hero_bg_2.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
         <div class="container">
           <div class="row align-items-center justify-content-center text-center">
 
@@ -142,144 +142,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       </div>  
 
     </div>
-
-
-    <div class="site-section">
-      
-      <div class="container overlap-section">
-        <div class="row">
-          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
-            <a href="#" class="unit-1 text-center">
-              <img src="<?php echo base_url() ?>assets/images/img_2.jpg" alt="Image" class="img-fluid">
-              <div class="unit-1-text">
-                <h3 class="unit-1-heading">Write Down Your Experience</h3>
-              </div>
-            </a>
-          </div>
-          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
-            <a href="#" class="unit-1 text-center">
-              <img src="<?php echo base_url() ?>assets/images/img_1.jpg" alt="Image" class="img-fluid">
-              <div class="unit-1-text">
-                <h3 class="unit-1-heading">Explore Asian Mountains</h3>
-              </div>
-            </a>
-          </div>
-          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
-            <a href="#" class="unit-1 text-center">
-              <img src="<?php echo base_url() ?>assets/images/img_3.jpg" alt="Image" class="img-fluid">
-              <div class="unit-1-text">
-                <h3 class="unit-1-heading">Safe Trip With Airasia</h3>
-              </div>
-            </a>
-          </div>
-        </div>
-      </div>
-    
-    </div>
-
-
-    <div class="site-section">
-      <div class="container">
-        <div class="row align-items-stretch">
-          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
-            <div class="unit-4 d-flex">
-              <div class="unit-4-icon mr-4"><span class="text-primary flaticon-airplane"></span></div>
-              <div>
-                <h3>Air Ticketing</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
-                <p><a href="#">Learn More</a></p>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
-            <div class="unit-4 d-flex">
-              <div class="unit-4-icon mr-4"><span class="text-primary flaticon-ship"></span></div>
-              <div>
-                <h3>Cruises</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
-                <p><a href="#">Learn More</a></p>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
-            <div class="unit-4 d-flex">
-              <div class="unit-4-icon mr-4"><span class="text-primary flaticon-route"></span></div>
-              <div>
-                <h3>Tour Packages</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
-                <p><a href="#">Learn More</a></p>
-              </div>
-            </div>
-          </div>
-
-        </div>
-      </div>
-    </div>
-  
-
-
-
-
-    
-    <div class="site-section block-13 bg-light">
-  
-
-    <div class="container">
-        <div class="row justify-content-center mb-5">
-          <div class="col-md-7">
-            <h2 class="font-weight-light text-black text-center">Testimonials</h2>
-          </div>
-        </div>
-
-        <div class="nonloop-block-13 owl-carousel">
-
-          <div class="item">
-            <div class="container">
-              <div class="row">
-                <div class="col-lg-6 mb-5">
-                  <img src="<?php echo base_url() ?>assets/images/img_1.jpg" alt="Image" class="img-md-fluid">
-                </div>
-                <div class="overlap-left col-lg-6 bg-white p-md-5 align-self-center">
-                  <p class="text-black lead">&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique dolorem quisquam laudantium, incidunt id laborum, tempora aliquid labore minus. Nemo maxime, veniam! Fugiat odio nam eveniet ipsam atque, corrupti porro&rdquo;</p>
-                  <p class="">&mdash; <em>James Martin</em>, <a href="#">Traveler</a></p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="item">
-            <div class="container">
-              <div class="row">
-                <div class="col-lg-6 mb-5">
-                  <img src="<?php echo base_url() ?>assets/images/img_2.jpg" alt="Image" class="img-md-fluid">
-                </div>
-                <div class="overlap-left col-lg-6 bg-white p-md-5 align-self-center">
-                  <p class="text-black lead">&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique dolorem quisquam laudantium, incidunt id laborum, tempora aliquid labore minus. Nemo maxime, veniam! Fugiat odio nam eveniet ipsam atque, corrupti porro&rdquo;</p>
-                  <p class="">&mdash; <em>Clair Augustin</em>, <a href="#">Traveler</a></p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="item">
-            <div class="container">
-              <div class="row">
-                <div class="col-lg-6 mb-5">
-                  <img src="<?php echo base_url() ?>assets/images/img_4.jpg" alt="Image" class="img-md-fluid">
-                </div>
-                <div class="overlap-left col-lg-6 bg-white p-md-5 align-self-center">
-                  <p class="text-black lead">&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique dolorem quisquam laudantium, incidunt id laborum, tempora aliquid labore minus. Nemo maxime, veniam! Fugiat odio nam eveniet ipsam atque, corrupti porro&rdquo;</p>
-                  <p class="">&mdash; <em>James Martin</em>, <a href="#">Traveler</a></p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-        </div>
-      </div>
-    </div>
-
-
 
     <div class="site-section">
       
@@ -354,20 +216,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <!-- <div class="site-section bg-light">
       
     </div> -->
-
-
-    <div class="site-blocks-cover overlay inner-page-cover" style="background-image: url(images/hero_bg_2.jpg); background-attachment: fixed;">
-      <div class="container">
-        <div class="row align-items-center justify-content-center text-center">
-
-          <div class="col-md-7" data-aos="fade-up" data-aos-delay="400">
-            <a href="https://vimeo.com/channels/staffpicks/93951774" class="play-single-big mb-4 d-inline-block popup-vimeo"><span class="icon-play"></span></a>
-            <h2 class="text-white font-weight-light mb-5 h1">Experience Our Outstanding Services</h2>
-            
-          </div>
-        </div>
-      </div>
-    </div>  
 
     <div class="site-section bg-light">
       <div class="container">
@@ -445,39 +293,73 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       </div>
     </div>
 
-
-    <div class="site-section">
+	<div class="site-blocks-cover overlay inner-page-cover" style="background-image: url(<?php echo base_url() ?>assets/images/hero_bg_2.jpg); background-attachment: fixed;">
       <div class="container">
-        <div class="row justify-content-center mb-5">
-          <div class="col-md-7 text-center">
-            <h2 class="font-weight-light text-black">Our Blog</h2>
-            <p class="color-black-opacity-5">See Our Daily News &amp; Updates</p>
-          </div>
-        </div>
-        <div class="row mb-3 align-items-stretch">
-          <div class="col-md-6 col-lg-6 mb-4 mb-lg-4">
-            <div class="h-entry">
-              <img src="<?php echo base_url() ?>assets/images/hero_bg_1.jpg" alt="Image" class="img-fluid">
-              <h2 class="font-size-regular"><a href="#">How to Plan Your Vacation</a></h2>
-              <div class="meta mb-4">by Theresa Winston <span class="mx-2">&bullet;</span> Jan 18, 2019 at 2:00 pm <span class="mx-2">&bullet;</span> <a href="#">News</a></div>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
-            </div> 
-          </div>
-          <div class="col-md-6 col-lg-6 mb-4 mb-lg-4">
-            <div class="h-entry">
-              <img src="<?php echo base_url() ?>assets/images/hero_bg_2.jpg" alt="Image" class="img-fluid">
-              <h2 class="font-size-regular"><a href="#">How to Plan Your Vacation</a></h2>
-              <div class="meta mb-4">by Theresa Winston <span class="mx-2">&bullet;</span> Jan 18, 2019 at 2:00 pm <span class="mx-2">&bullet;</span> <a href="#">News</a></div>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-12 text-center">
-            <a href="#" class="btn btn-outline-primary border-2 py-3 px-5">View All Blog Posts</a>
+        <div class="row align-items-center justify-content-center text-center">
+
+          <div class="col-md-7" data-aos="fade-up" data-aos-delay="400">
+            <a href="https://vimeo.com/channels/staffpicks/93951774" class="play-single-big mb-4 d-inline-block popup-vimeo"><span class="icon-play"></span></a>
+            <h2 class="text-white font-weight-light mb-5 h1">Experience Our Outstanding Services</h2>
+            
           </div>
         </div>
       </div>
+    </div>  
+
+	<div class="site-section block-13 bg-light">
+		<div class="container">
+			<div class="row justify-content-center mb-5">
+			<div class="col-md-7">
+				<h2 class="font-weight-light text-black text-center">Testimonials</h2>
+			</div>
+			</div>
+
+			<div class="nonloop-block-13 owl-carousel">
+
+			<div class="item">
+				<div class="container">
+				<div class="row">
+					<div class="col-lg-6 mb-5">
+					<img src="<?php echo base_url() ?>assets/images/img_1.jpg" alt="Image" class="img-md-fluid">
+					</div>
+					<div class="overlap-left col-lg-6 bg-white p-md-5 align-self-center">
+					<p class="text-black lead">&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique dolorem quisquam laudantium, incidunt id laborum, tempora aliquid labore minus. Nemo maxime, veniam! Fugiat odio nam eveniet ipsam atque, corrupti porro&rdquo;</p>
+					<p class="">&mdash; <em>James Martin</em>, <a href="#">Traveler</a></p>
+					</div>
+				</div>
+				</div>
+			</div>
+
+			<div class="item">
+				<div class="container">
+				<div class="row">
+					<div class="col-lg-6 mb-5">
+					<img src="<?php echo base_url() ?>assets/images/img_2.jpg" alt="Image" class="img-md-fluid">
+					</div>
+					<div class="overlap-left col-lg-6 bg-white p-md-5 align-self-center">
+					<p class="text-black lead">&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique dolorem quisquam laudantium, incidunt id laborum, tempora aliquid labore minus. Nemo maxime, veniam! Fugiat odio nam eveniet ipsam atque, corrupti porro&rdquo;</p>
+					<p class="">&mdash; <em>Clair Augustin</em>, <a href="#">Traveler</a></p>
+					</div>
+				</div>
+				</div>
+			</div>
+
+			<div class="item">
+				<div class="container">
+				<div class="row">
+					<div class="col-lg-6 mb-5">
+					<img src="<?php echo base_url() ?>assets/images/img_4.jpg" alt="Image" class="img-md-fluid">
+					</div>
+					<div class="overlap-left col-lg-6 bg-white p-md-5 align-self-center">
+					<p class="text-black lead">&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique dolorem quisquam laudantium, incidunt id laborum, tempora aliquid labore minus. Nemo maxime, veniam! Fugiat odio nam eveniet ipsam atque, corrupti porro&rdquo;</p>
+					<p class="">&mdash; <em>James Martin</em>, <a href="#">Traveler</a></p>
+					</div>
+				</div>
+				</div>
+			</div>
+
+			</div>
+		</div>
     </div>
     
     <div class="site-section border-top">
