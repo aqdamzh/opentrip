8825430e5c3f3b95ce8d3180f50070e06fa7bef9
aqdamzh <aqdamzain1@gmail.com>
2019-12-27 10:35:11 +0700

    first deploy

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/opentrip/';
+$config['base_url'] = 'http://famstraveller.azurewebsites.net/';
 
 /*
 |--------------------------------------------------------------------------
